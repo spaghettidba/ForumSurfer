f4f650de9411056b02301143d9ff38bb20d9a3fb
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2016-08-31 11:47:08 +0200

    Edited version with hyperlink

--- ForumSurfer/View/MainWindow.xaml.cs
+++ ForumSurfer/View/MainWindow.xaml.cs
@@ -172,5 +172,11 @@ namespace ForumSurfer
         }
 
 
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
     }
 }
